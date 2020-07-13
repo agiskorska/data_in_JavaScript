e3aa033427d2c0f66fee067d1eef9c3165507886
Gingertonic <bethmschofield@gmail.com>
2020-07-13 11:44:34 +0100

    add telescope demo

---
+++ scope.js
@@ -0,0 +1,20 @@
+function telescope(){
+    var a = "near";
+    let b = "near";
+    const c = "near";
+    d = "near";
+
+    if (true){
+        var a = "far";
+        let b = "far";
+        const c = "far";
+        d = "far";
+    };
+
+    console.log(`a is ${a}.`);
+    console.log(`b is ${b}.`);
+    console.log(`c is ${c}.`);
+    console.log(`d is ${d}.`);
+};
+
+telescope();
