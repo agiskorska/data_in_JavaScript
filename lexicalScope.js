8c79aa4123e05763285a1526fb346a77235fb35a
getfutureproof-admin <admin@getfutureproof.co.uk>
2020-11-02 16:37:52 +0000

    add inner vars to lexicalScope

--- lexicalScope.js
+++ lexicalScope.js
@@ -1,25 +1,29 @@
 var intro = "Hello!"
+var item 
 
 function outerCircle(){
     var message = "You have access to the outer circle!";
-    console.log("\nYou have entered the outer circle!\n******")
-    console.log(intro);
-    console.log(message);
-    console.log(secretMessage);
-
+    item = "thing"
+    // console.log("\nYou have entered the outer circle!\n******")
+    // console.log(intro);
+    // console.log(message);
+    // console.log(secretMessage);
+    
     function innerCircle(){
         var secretMessage = "You have access to the inner circle!";
+        var intro = "Inner intro"
         console.log("\nYou have entered the inner circle!\n******")
         console.log(intro);
         console.log(message);
         console.log(secretMessage);
     }
-
     innerCircle();
 };
 
-// console.log(intro);
-// console.log(message);
-// console.log(secretMessage);
 
 // outerCircle();
+
+// console.log(intro);
+// console.log(message);
+// console.log(item)
+// console.log(secretMessage);
